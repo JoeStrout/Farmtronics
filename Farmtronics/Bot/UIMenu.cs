b433373e75572fb0b45dadae699fef345d9c5158
arannya <arannya.monzur@gmail.com>
2024-05-05 20:34:18 +0600

    [Fix] Attempt at fudging console boundaries

--- Farmtronics/Bot/UIMenu.cs
+++ Farmtronics/Bot/UIMenu.cs
@@ -6,6 +6,7 @@ It has several parts:
 	3. A MiniScript console.
 */
 
+using System.Threading;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -191,11 +192,10 @@ namespace Farmtronics.Bot {
 		/// Return whether the given screen position is in our draggable area.
 		/// </summary>
 		bool inDragArea(int x, int y) {
-			// ModEntry.instance.Monitor.Log($"inDragArea: x={x} y={y}, in botInventoryBounds: {botInventoryBounds().Contains(x, y)}, in consoleBounds: {consoleBounds().Contains(x, y)}");
+			//ModEntry.instance.Monitor.Log($"inDragArea: x={x} y={y}, in botInventoryBounds: {botInventoryBounds().Contains(x, y)}, in consoleBounds: {consoleBounds().Contains(x, y)}");
 			if (botInventoryBounds().Contains(x,y)) return false;
-			//if (consoleBounds().Contains(x,y)) return false;
-			// Note: Console bounds are not being calculated correctly in SDV 1.6
-			
+			if (consoleBounds().Contains(x+30,y-90)) return false;
+
 			var playerInv = inventory;
 			Rectangle invRect = new Rectangle(playerInv.xPositionOnScreen, playerInv.yPositionOnScreen, width, height);
 			if (invRect.Contains(x,y)) return false;
